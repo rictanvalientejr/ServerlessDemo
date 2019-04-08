b73a152bb0d362eb3ed8636da589176d01d9f366
rictanvalientejr <rictanvalientejr@outlook.com>
2019-04-08 20:15:06 +0800

    sns

--- ServerlessDemo/SendSMS_Basic.js
+++ ServerlessDemo/SendSMS_Basic.js
@@ -2,9 +2,37 @@ let AWS = require('aws-sdk');
 const sns = new AWS.SNS();
 
 exports.handler = function (event, context, callback) {
-    console.log("value1 = " + event.mobile);
-    console.log("value2 = " + event.key2); 
+    var receiver = event.receiver;
+    var sender = event.sender;
+    var message = event.message;
 
-  
+    console.log("Sending SMS ...");
+    console.log("sender = " + event.sender);
+    console.log("receiver = " + event.receiver);
+    console.log("message = " + event.message);
+    
+
+    sns.publish({
+        Message: message,
+        MessageAttributes: {
+            'AWS.SNS.SMS.SMSType': {
+                DataType: 'String',
+                StringValue: 'Promotional'
+            },
+            'AWS.SNS.SMS.SenderID': {
+                DataType: 'String',
+                StringValue: sender
+            },
+        },
+        PhoneNumber: receiver
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+    console.log("SMS Sent...");
 }
 
