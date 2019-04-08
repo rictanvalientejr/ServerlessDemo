23eae0624d821e32c7b480f05c3a5324a8796159
rictanvalientejr <rictanvalientejr@outlook.com>
2019-04-08 20:09:39 +0800

    1

--- ServerlessDemo/SendSMS_Basic.js
+++ ServerlessDemo/SendSMS_Basic.js
@@ -5,19 +5,6 @@ exports.handler = function (event, context, callback) {
     console.log("value1 = " + event.mobile);
     console.log("value2 = " + event.key2); 
 
-    sns.publish({
-        Message: 'hello',
-        Subject: 'Test',
-        MessageAttributes: {},
-        MessageStructure: 'String',
-        TopicArn: 'arn:aws:sns:ap-southeast-1:137351355934:DTI_Demo'
-    }).promise()
-        .then(data => {
-            // your code goes here
-        })
-        .catch(err => {
-            // error handling goes here
-            callback(error);
-        });
+  
 }
 
